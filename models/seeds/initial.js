d08ee2cb52498a748a58f6c37643e5e3c88d97fe
sidiousvic <victor6rene@gmail.com>
2020-09-21 14:13:25 +0900

    seed initial notes

--- models/seeds/initial.js
+++ models/seeds/initial.js
@@ -1,13 +1,11 @@
+const initialNotes = require("./data/notes.json");
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("notes")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("notes").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" },
-      ]);
+      return knex("notes").insert(initialNotes);
     });
 };
