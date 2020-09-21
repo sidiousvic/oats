11cefb3d309116a7dc1b291f5f93ea355d507c96
sidiousvic <victor6rene@gmail.com>
2020-09-21 16:16:14 +0900

    add DELETE api/notes/:id

--- controllers/index.js
+++ controllers/index.js
@@ -7,4 +7,15 @@ router.get("/notes", async (_, res) => {
   res.send(notes);
 });
 
+router.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+  await db("notes")
+    .where({
+      id: id,
+    })
+    .delete();
+  res.send(`Note with ID ${id} was deleted.`);
+});
+
 module.exports = router;
