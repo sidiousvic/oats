8edca4f86f027f6ca86acc5780915f55ed38a5a6
sidiousvic <victor6rene@gmail.com>
2020-09-21 14:47:17 +0900

    script for initial migration/seed

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
 const Knex = require("knex");
+const knexfile = require("./knexfile");
+const knex = Knex(knexfile);
 
-const knex = Knex();
+knex.migrate
+  .latest()
+  .then(() => {
+    return knex.seed.run();
+  })
+  .catch((err) => {
+    console.log(err);
+    throw new Error("Could not perform migrations and seeds.");
+  })
+  .then(() => {
+    console.log("Migrations ran successfully! ✅");
+  });
 
 module.exports = knex;
