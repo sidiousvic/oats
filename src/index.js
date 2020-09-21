a4157dbb0b0d44d216f13b1886be263c42bcd7c9
sidiousvic <victor6rene@gmail.com>
2020-09-21 14:48:27 +0900

    testing GET api/notes on fe

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 // test proxy to express server @ 4000
-fetch("/api/hello")
+fetch("/api/notes")
   .then((res) => {
-    console.warn(res);
     return res.json();
   })
-  .then((json) => console.warn(json));
+  .then((json) => console.warn("Notes from the database: ", json));
 
 ReactDOM.render(<h1>Hello Assessment!</h1>, document.getElementById("root"));
