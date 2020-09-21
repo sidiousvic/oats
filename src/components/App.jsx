903b031165e6cc0da36b67fed29d18618ed550a4
sidiousvic <victor6rene@gmail.com>
2020-09-21 14:57:42 +0900

    rendering notes @ frontend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
+import React, { useEffect, useState } from "react";
+
 export default function App() {
-  return "This is the App component.";
+  const [notes, setNotes] = useState([]);
+
+  async function getNotes() {
+    const response = await fetch("/api/notes");
+    const notes = await response.json();
+    setNotes(notes);
+  }
+
+  useEffect(getNotes, []);
+
+  return notes.map((note) => (
+    <div key={note.title}>
+      <h1>{note.title}</h1>
+      <p>{note.body}</p>
+    </div>
+  ));
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/App";
 
-// test proxy to express server @ 4000
-fetch("/api/notes")
-  .then((res) => {
-    return res.json();
-  })
-  .then((json) => console.warn("Notes from the database: ", json));
-
 ReactDOM.render(<App />, document.getElementById("root"));
