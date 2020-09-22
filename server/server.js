13346c35aa7fec83490301876d326325498a97b3
sidiousvic <victor6rene@gmail.com>
2020-09-22 23:31:19 +0900

    morgan combined setting

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const api = require("../controllers");
 
-app.use(morgan());
+app.use(morgan("combined"));
 app.use("/api", express.json(), express.urlencoded({ extended: true }), api);
 
 module.exports = app;
