0de97e44b6acb16f389976879188ce067694ed7e
sidiousvic <victor6rene@gmail.com>
2020-09-22 16:01:07 +0900

    divide stylesheet mobile <> desktop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import "./styles.css";
+import "./styles.desktop.css";
+import "./styles.mobile.css";
 import Oats from "./components/Oats";
 
 ReactDOM.render(<Oats />, document.getElementById("root"));
