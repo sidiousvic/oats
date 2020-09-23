db37a1113f7c74083e27a3caf201ecac278f8ce8
sidiousvic <victor6rene@gmail.com>
2020-09-23 19:50:03 +0900

    notesCache on FE > upd. DB on Note blur

--- src/components/Oats.jsx
+++ src/components/Oats.jsx
@@ -2,41 +2,29 @@ import React, { useState, useEffect, createContext } from "react";
 import Sidebar from "./Sidebar";
 import Note from "./Note";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 export const OatsContext = createContext();
 const OatsProvider = OatsContext.Provider;
 
 export default function Oats() {
-  const [notes, setNotes] = useState([]);
-  const [activeNote, setActiveNote] = useState({});
+  const [notesCache, setNotesCache] = useState([]);
+  const [activeNoteId, setActiveNoteId] = useState("0");
   const [savedNotesAt, setSavedNotesAt] = useState(Date.now());
 
+  function getActiveNote(id) {
+    const activeNote = notesCache.find((note) => note.id === id);
+    return activeNote;
+  }
+
   useEffect(() => {
-    async function getAndSetNotes() {
+    async function getNotes() {
       const response = await axios.get("/oats/notes");
       const notes = response.data;
-      setNotes(notes);
+      setNotesCache(notes);
     }
-    getAndSetNotes();
-  }, [savedNotesAt]);
-
-  useEffect(() => {
-    if (!notes.length) {
-      setActiveNote({
-        title: "Welcome to Oats! 👽 ✏️",
-        body:
-          "A slick notes app.                                      \n\n👈🏼 Select a note\n❌ Delete a note\n💾 Save your note",
-        id: "0",
-      });
-    }
-  }, [notes]);
-
-  useEffect(() => {
-    if (notes.length) {
-      const lastNote = notes[notes.length - 1];
-      if (activeNote.id === lastNote.id) setActiveNote(lastNote);
-    }
-  }, [notes, activeNote.id]);
+    getNotes();
+  }, []);
 
   async function addNote() {
     const newNote = {
@@ -45,41 +33,45 @@ export default function Oats() {
       body: "This is a new note.",
     };
     await axios.post(`/oats/notes`, newNote);
-    setSavedNotesAt(Date.now());
+    setNotesCache([...notesCache, newNote]);
+    setActiveNoteId(newNote.id);
   }
   async function deleteNote(id) {
     await axios.delete(`/oats/notes/${id}`);
+    setNotesCache(notesCache.filter((note) => note.id !== id));
     setSavedNotesAt(Date.now());
-  }
-  function autoSaveNote() {
-    const autoSaveInterval = setInterval(() => {
-      if (activeNote.id) updateNote(activeNote.id);
-    }, 3000);
-    return () => clearInterval(autoSaveInterval);
+    setActiveNoteId(notesCache[notesCache.length - 1]);
   }
   async function updateNote(id) {
+    const activeNote = getActiveNote(id);
     await axios.patch(`/oats/notes/${id}`, activeNote);
+    setNotesCache(
+      notesCache.map((note) => {
+        if (note.id === id) return activeNote;
+        else return note;
+      })
+    );
     setSavedNotesAt(Date.now());
   }
 
   const OatsAPI = {
-    notes,
-    activeNote,
-    setActiveNote,
-    setNotes,
+    notesCache,
+    getActiveNote,
+    activeNoteId,
+    setActiveNoteId,
+    setNotesCache,
     savedNotesAt,
     setSavedNotesAt,
     addNote,
     deleteNote,
-    autoSaveNote,
     updateNote,
   };
 
   return (
     <OatsProvider value={OatsAPI}>
       <div id="oats">
-        <Sidebar notes={notes} activeNote={activeNote} />
-        <Note activeNote={activeNote} />
+        <Sidebar />
+        <Note />
       </div>
     </OatsProvider>
   );
