80209d5f0b0bf5e96fd63b20ee9f98f12dc2520a
sidiousvic <victor6rene@gmail.com>
2020-09-24 08:00:26 +0900

    mv index.js > index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import "./styles.desktop.css";
-import "./styles.mobile.css";
+import "./styles/styles.global.css";
 import Oats from "./components/Oats";
 
 ReactDOM.render(<Oats />, document.getElementById("root"));
