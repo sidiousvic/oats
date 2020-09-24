f6926d46e84fce88c7e92b101c2a11737d4f6037
sidiousvic <victor6rene@gmail.com>
2020-09-24 23:53:40 +0900

    Sidebar orientation—reactive

--- src/components/Oats/Sidebar/index.jsx
+++ src/components/Oats/Sidebar/index.jsx
@@ -1,26 +1,46 @@
 import React, { useContext } from "react";
 import { OatsContext } from "../../Oats";
 import SidebarNotes from "./SidebarNotes";
-import AddNoteButton from "./AddNoteButton";
+import Button from "./Button";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Sidebar() {
-  const { addNote } = useContext(OatsContext);
-  const [touched, setTouched] = useState(false);
+  const { addNote, activeNoteId } = useContext(OatsContext);
+  const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+  const [orientedLandscape, setOrientedLandscape] = useState(
+    ~~window.screen.orientation.angle === 90
+  );
+  const sideBarExitEmoji = activeNoteId === "0" ? "🔙" : "✏️";
+
+  function closeMobileSidebar() {
+    setMobileSidebarOpen(false);
+    setOrientedLandscape(~~window.screen.orientation.angle === 90);
+  }
+
+  function openMobileSidebar() {
+    setMobileSidebarOpen(true);
+    setOrientedLandscape(~~window.screen.orientation.angle === 90);
+  }
+
+  function closeSideBarOnOrientationChange() {
+    window.addEventListener("orientationchange", closeMobileSidebar);
+    return () =>
+      window.removeEventListener("orientationchange", closeMobileSidebar);
+  }
+
+  useEffect(closeSideBarOnOrientationChange, []);
 
   return (
     <>
       <div
         id="sidebar"
         role="textbox"
-        className={touched ? "sidebar-touched" : ""}
+        className={mobileSidebarOpen ? "mobile-sidebar-open" : ""}
         tabIndex="0"
         onKeyDown={() => {}} // TODO: Accessiblity
-        onFocus={() => {
-          setTouched(true);
-        }}
-        onMouseLeave={() => {
-          setTouched(false);
+        onTouchStart={() => {
+          if (!orientedLandscape) openMobileSidebar();
         }}
       >
         <h1 id="sidebar-title">Oats</h1>
@@ -28,7 +48,13 @@ export default function Sidebar() {
           by <a href="https://www.github.com/sidiousvic">sidiousvic</a>
         </h3>
         <SidebarNotes />
-        <AddNoteButton emoji={"+"} onClickHandler={addNote} />
+        <Button emoji={"🌾"} onClickHandler={addNote} />
+        {mobileSidebarOpen && (
+          <Button
+            emoji={sideBarExitEmoji}
+            onClickHandler={closeMobileSidebar}
+          />
+        )}
       </div>
     </>
   );
