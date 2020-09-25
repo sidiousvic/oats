b51eb2f6d336017c9437d5743bf421a9260a0507
sidiousvic <victor6rene@gmail.com>
2020-09-25 15:49:15 +0900

    implement Note type

--- server/routers/notes.ts
+++ server/routers/notes.ts
@@ -1,26 +1,25 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 const notesRouter = express.Router();
 const db = require("../models/db");
 
 type Note = {
-  noteId: number;
+  noteId?: number;
   id: string;
   title: string;
   body: string;
-  timestamp: Date;
+  timestamp?: Date;
 };
 
 notesRouter.get("/notes", async (_, res: Response) => {
-  const notes = await db
+  const notes: Note = await db
     .select()
     .from("notes")
     .orderBy("timestamp");
-  console.log(notes);
   res.send(notes);
 });
 
-notesRouter.delete("/notes/:id", async (req, res) => {
-  const id = req.params.id;
+notesRouter.delete("/notes/:id", async (req: Request, res: Response) => {
+  const id: string = req.params.id;
   await db("notes")
     .where({ id })
     .delete();
@@ -29,17 +28,17 @@ notesRouter.delete("/notes/:id", async (req, res) => {
 
 notesRouter.post("/notes", async (req, res) => {
   const { title, body, id } = req.body;
-  const note = {
+  const incomingNewNote: Note = {
     id,
     title,
     body,
   };
-  await db("notes").insert(note);
+  await db("notes").insert(incomingNewNote);
   res.send(`Note with ID ${id} was added.`);
 });
 
 notesRouter.patch("/notes/:id", async (req, res) => {
-  const note = req.body;
+  const note: Note = req.body;
   const { id } = req.params;
   await db("notes")
     .where({ id })
